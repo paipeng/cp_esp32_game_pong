30ab98d06f4375bc7ec5dbcc5b18b084691ab3c7
Pai Peng <sipaipv6@gmail.com>
2023-11-05 21:50:26 +0800

    AiEsp32RotaryEncoder

--- cp_rotate_button.cpp
+++ cp_rotate_button.cpp
@@ -1,21 +1,53 @@
 #include "cp_rotate_button.h"
 
+
 #define CP_GPIO_DT 2
 #define CP_GPIO_CLK 4
 #define CP_GPIO_SW 35
+#define CP_GPIO_VCC -1	 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+#define CP_GPIO_STEPS 1
 
-CPRotateButton::CPRotateButton():clk(HIGH) {
+AiEsp32RotaryEncoder *encoder = NULL;
 
+void IRAM_ATTR readEncoderISR() {
+  if (encoder) {
+  	encoder->readEncoder_ISR();
+  }
+}
+
+CPRotateButton::CPRotateButton():clk(HIGH), 
+rotaryEncoder(CP_GPIO_CLK, CP_GPIO_DT, CP_GPIO_SW, CP_GPIO_VCC, CP_GPIO_STEPS) {
+  encoder = &rotaryEncoder;
 }
 
 void CPRotateButton::init() {
   Serial.println("CPRotateButton::init");
+#if 0
   pinMode(CP_GPIO_CLK, INPUT);
   pinMode(CP_GPIO_DT, INPUT);
   pinMode(CP_GPIO_SW, INPUT);
+#else
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+
+  	//set boundaries and if values should cycle or not
+	//in this example we will set possible values between 0 and 1000;
+	bool circleValues = false;
+	rotaryEncoder.setBoundaries(0, 1000, circleValues); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+
+	/*Rotary acceleration introduced 25.2.2021.
+   * in case range to select is huge, for example - select a value between 0 and 1000 and we want 785
+   * without accelerateion you need long time to get to that number
+   * Using acceleration, faster you turn, faster will the value raise.
+   * For fine tuning slow down.
+   */
+	//rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
+	rotaryEncoder.setAcceleration(250); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
+#endif
 }
 
 int CPRotateButton::read() {
+#if 0  
   int b = digitalRead(CP_GPIO_SW);
   if (b == LOW) {
     Serial.println("button pressed");
@@ -38,4 +70,20 @@ int CPRotateButton::read() {
     }
   }
   return 0;
+#else
+  if (rotaryEncoder.encoderChanged()) {
+    Serial.print(getFrequency(), 1);
+    Serial.println(" MHz ");
+  }
+  if (rotaryEncoder.isEncoderButtonClicked()) {
+    Serial.print("Radio station set to ");
+    Serial.print(getFrequency());
+    Serial.println(" MHz ");
+  }
+  return 0;
+#endif  
+}
+
+float CPRotateButton::getFrequency() {
+    return (float)rotaryEncoder.readEncoder() / 10.0;
 }
--- cp_rotate_button.h
+++ cp_rotate_button.h
@@ -2,6 +2,7 @@
 #define _CP_ROTATE_BUTTON_H_
 
 #include <Arduino.h>
+#include "AiEsp32RotaryEncoder.h"
 
 class CPRotateButton {
 public:
@@ -9,7 +10,11 @@ public:
   void init();
   int read();
 private:
+  float getFrequency();
+
+private:
   int clk;
+  AiEsp32RotaryEncoder rotaryEncoder;
 };
 
 #endif
