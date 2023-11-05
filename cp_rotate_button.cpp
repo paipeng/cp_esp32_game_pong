31ab9416e9cac936e3f7c103afd876bec25029fd
Pai Peng <sipaipv6@gmail.com>
2023-11-05 21:34:27 +0800

    button press works, only counter clock wise. bug to fix

--- cp_rotate_button.cpp
+++ cp_rotate_button.cpp
@@ -16,17 +16,25 @@ void CPRotateButton::init() {
 }
 
 int CPRotateButton::read() {
+  int b = digitalRead(CP_GPIO_SW);
+  if (b == LOW) {
+    Serial.println("button pressed");
+    clk = HIGH;
+  }
   int new_clk = digitalRead(CP_GPIO_CLK);
   if (new_clk != clk) {
     // changed
     clk = new_clk;
     int value = digitalRead(CP_GPIO_DT);
-    if (clk == LOW && value == HIGH) {
+    Serial.printf("new_clk: %d clk: %d value: %d\n", new_clk, clk, value);
+    if (new_clk == LOW && value == HIGH) {
       Serial.println("rotate clockwise");
       return 1;
-    } else if (clk == LOW && value == LOW) {
+    } else if (new_clk == LOW && value == LOW) {
       Serial.println("rotate counter-clockwise");
       return -1;
+    } else {
+      Serial.printf("NO defined -> new_clk: %d clk: %d value: %d\n", new_clk, clk, value);
     }
   }
   return 0;
