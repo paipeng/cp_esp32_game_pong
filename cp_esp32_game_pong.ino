349a462c0ab0dcff7138d0991ed76867b71aef35
Pai Peng <sipaipv6@gmail.com>
2023-11-16 23:37:09 +0800

    update

--- cp_esp32_game_pong.ino
+++ cp_esp32_game_pong.ino
@@ -4,9 +4,11 @@
 #include "cp_ps2_joystick.h"
 #include "cp_audio.h"
 #include "cp_rotate_button.h"
+#include "cp_lcd128x64_display.h"
 
 CPSnake snake;
-CPGFXDisplay display;
+CPLCD128X64Display display;
+//CPGFXDisplay display;
 CPPS2Joystick joystick;
 CPAudio audio;
 CPRotateButton rotateButton;
@@ -19,12 +21,14 @@ void setup() {
   // init
   audio.init();
   display.init();
-  joystick.init();
-  snake.init(display.get_size());
+  //joystick.init();
+  //snake.init(display.get_size());
   rotateButton.init();
 
   game_over = 0;
   delay(5000); // 5 seconds
+
+  display.drawText("TEST", 10, 10);
 }
 
 
@@ -34,39 +38,4 @@ void loop() {
   if (rotate != 0) {
     Serial.printf("rotate: %d\n", rotate);
   }
-#if 1
-  int event = joystick.readMove();
-  if (event == BUTTON_DOWN) {
-    Serial.println("reset");
-    game_over = 0;
-    display.reset();
-    snake.reset();
-    display.update_draw(snake.get_points(), snake.get_random_point());
-    delay(500);
-  } else {
-    if (game_over == 0) {
-      int state = snake.update_snake(event);
-      if (state == 2) {
-        game_over = 0;
-        audio.beep();
-      } else {
-        game_over = state;
-      }
-      if (game_over == 0) {
-        display.update_draw(snake.get_points(), snake.get_random_point());
-      }
-      delay(400);
-    } else {
-      if (game_over == 1) {
-        Serial.println("game_over");
-        display.game_over(snake.get_score());
-        game_over = 2;
-      } else {
-
-      }
-      delay(50);
-    }
-
-  }
-#endif
 }
